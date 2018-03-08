468037b6c2ccdc7a35bf39e7fea788f765667adc
FernandaLopezRodriguez <35509634+FernandaLopezRodriguez@users.noreply.github.com>
2018-03-07 21:20:16 -0600

    Practica

--- Models/Tarjeta.cs
+++ Models/Tarjeta.cs
@@ -24,7 +24,26 @@ namespace CreditoWeb.Models
         /// como estamos dentro de la clase de tarjeta tenemos acceso a la propiedad TarjetaNum 
         private bool esValida()
         {
-            return false;
+            int suma=0, par=0;
+            for (int i=1; i<=TarjetaNum.Length; i+=2)
+            {
+                string aux = TarjetaNum.Substring(i,1);
+                par = Int32.Parse (aux);
+                if (par >=10)
+                {
+                    suma+=par-9;
+                }
+                else{
+                    suma+=par;
+                }
+            }
+            for (int n=0; n<=TarjetaNum.Length; n+=2)
+            {
+                int impar = Int32.Parse (TarjetaNum.Substring(n,1));
+                suma+=impar;
+            }
+            Valida = ( suma % 10 == 0);
+            return Valida;
         }
 
 
@@ -32,7 +51,24 @@ namespace CreditoWeb.Models
         /// como estamos dentro de la clase de tarjeta tenemos acceso a la propiedad TarjetaNum 
         private TipoTarjeta tipoDeTarjeta()
         {
-            return TipoTarjeta.NOVALIDA;
+            int inicio = (Int32.Parse(TarjetaNum.Substring(0,2)));
+            int v = (Int32.Parse(TarjetaNum.Substring(0,1)));
+            if (TarjetaNum.Length==15&&(inicio==34||inicio==37))
+            {
+            return TipoTarjeta.AMERICANEXPRESS;
+            }            
+            else if (TarjetaNum.Length==16&&(inicio==51||inicio==52||inicio==53||inicio==54||inicio==55))
+            {
+                return TipoTarjeta.MASTERCARD;
+            }
+            else if ((TarjetaNum.Length==13||TarjetaNum.Length==16)&&v==4)
+            {
+                return TipoTarjeta.VISA;
+            }
+            else
+            {
+                return TipoTarjeta.NOVALIDA;
+            }
         }
 
 
